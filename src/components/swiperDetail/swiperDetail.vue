3b0a21299b27513a1194f12af47e808794880402
chenxiangkai <lvy.123@foxmail.com>
2018-04-26 15:04:53 +0800

    ...

--- src/components/swiperDetail/swiperDetail.vue
+++ src/components/swiperDetail/swiperDetail.vue
@@ -1,36 +1,22 @@
 <template>
-  <transition name="swiper-detail">
-    <div class="swiper-detail">
-      <div class="title">
-        <my-title :title="'平台消息'"></my-title>
-      </div>
-      <scroll ref="scroll" class="scroll">
-        <div>
-          <div class="detail-content" v-if="$route.query.type == 1">
-            <img :src="bg" @load="loadImage" alt="">
-          </div>
-          <div class="detail-content" v-if="$route.query.type == 2">
-            <img :src="relay1" @load="loadImage" alt="">
-          </div>
-          <div class="detail-content" v-if="$route.query.type == 2">
-            <img :src="relay2" @load="loadImage" alt="">
-          </div>
-          <div class="detail-content" v-if="$route.query.type == 2">
-            <img :src="relay3" @load="loadImage" alt="">
-          </div>
-          <div class="detail-content" v-if="$route.query.type == 2">
-            <img :src="relay4" @load="loadImage" alt="">
-          </div>
-        </div>
-      </scroll>
+  <div class="swiper-detail">
+    <div class="title">
+      <my-title :title="'平台消息'"></my-title>
     </div>
-  </transition>
+    <!-- <scroll ref="scroll" class="scroll"> -->
+    <div v-html="html" id="img">
+    </div>
+    <!-- </scroll> -->
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
+import { getrotation } from 'api/swiperDetail'
 import MyTitle from 'base/title/title'
 import Scroll from 'base/scroll/scroll'
 import { countNum } from 'api/bannerDetail'
+import TYPE from 'common/js/buryingpointType'
+import { addLog } from 'api/buryingpoint'
 export default {
   components: {
     MyTitle,
@@ -38,29 +24,29 @@ export default {
   },
   data() {
     return {
-      bg: require('common/image/swiper1.jpg'),
-      relay1: require('common/image/relay1.jpg'),
-      relay2: require('common/image/relay2.jpg'),
-      relay3: require('common/image/relay3.jpg'),
-      relay4: require('common/image/relay4.jpg')
+      html: ''
     }
   },
   created() {
+    getrotation(this.$route.query.id).then(res => {
+      this.html = res.data.data
+    })
+    // .then(() => {
+    //   // this.$refs.scroll.refresh()
+    // })
+    const self = this
+    setTimeout(function() {
+      if (parseInt(self.$route.query.type) === 1) {
+        addLog(TYPE.SWIPERDETAIL1, '', '', '', window.USERMSG)
+      }
+      if (parseInt(self.$route.query.type) === 2) {
+        addLog(TYPE.SWIPERDETAIL2, '', '', '', window.USERMSG)
+      }
+    }, 1500)
     this._countNum()
   },
+  updated() {},
   methods: {
-    showProject() {
-      this.$emit('showProject')
-    },
-    loadImage() {
-      if (!this.checkloaded) {
-        this.checkloaded = true
-      }
-      this.$refs.scroll.refresh()
-    },
-    callPhone() {
-      window.location.href = 'tel: 021-6298-5128'
-    },
     _countNum() {
       countNum().then(res => {})
     }
@@ -68,15 +54,16 @@ export default {
 }
 </script>
 
-<style scoped lang="stylus" rel="stylesheet/stylus">
+<style lang="stylus" rel="stylesheet/stylus">
 @import '~common/stylus/variable';
 
 .swiper-detail {
-  position: fixed;
+  padding-top: 50px;
+  // position: fixed;
   width: 100%;
-  top: 0px;
-  bottom: 0;
-  z-index: 10000;
+  // top: 0px;
+  // bottom: 0;
+  // z-index: 10000;
   background: #eee;
   font-size: $font-size-medium;
 
